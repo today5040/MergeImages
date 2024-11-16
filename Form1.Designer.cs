3d0616b50f4703d5da9e79550da0b12a2bb91e57
梦半闲 <today5040@hotmail.com>
2024-11-16 10:57:17 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace MergeImages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnChooseFolder = new System.Windows.Forms.Button();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择要合并图片的文件夹";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.BackColor = System.Drawing.Color.White;
+            this.txtFolder.Location = new System.Drawing.Point(29, 47);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(250, 21);
+            this.txtFolder.TabIndex = 1;
+            // 
+            // btnChooseFolder
+            // 
+            this.btnChooseFolder.Location = new System.Drawing.Point(279, 46);
+            this.btnChooseFolder.Name = "btnChooseFolder";
+            this.btnChooseFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnChooseFolder.TabIndex = 2;
+            this.btnChooseFolder.Text = "选择文件夹";
+            this.btnChooseFolder.UseVisualStyleBackColor = true;
+            this.btnChooseFolder.Click += new System.EventHandler(this.btnChooseFolder_Click);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(29, 74);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(325, 23);
+            this.btnMerge.TabIndex = 3;
+            this.btnMerge.Text = "开始合并";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "处理结果：";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(31, 119);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(323, 130);
+            this.txtResult.TabIndex = 5;
+            this.txtResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.btnChooseFolder);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(400, 300);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "合并图片 For ZhouZhou";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnChooseFolder;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MergeImages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseFolder_Click(object sender, EventArgs e)
+        {
+            // 创建 FolderBrowserDialog 对象
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                // 设置初始目录
+                folderBrowserDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // 显示对话框
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // 获取用户选择的路径
+                    txtFolder.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+        private async void btnMerge_Click(object sender, EventArgs e)
+        {
+            var folder = txtFolder.Text;
+            if (string.IsNullOrWhiteSpace(folder))
+            {
+                MessageBox.Show("请先选择文件夹");
+                return;
+            }
+            txtResult.AppendText("所选文件夹" + folder + ";开始检测文件...\r\n");
+            var files = Directory.GetFiles(folder);
+            if (files.Length == 0)
+            {
+                txtResult.AppendText("文件夹" + folder + "未发现文件，已停止处理。\r\n");
+                return;
+            }
+            txtResult.AppendText("文件夹" + folder + "检测到" + files.Length + "个文件，禁止文件夹中存放非图片文件，开始处理。\r\n");
+            try
+            {
+                // 使用 Task 在后台线程执行处理
+                await Task.Run(() =>
+                {
+                    string[] imageFiles = files.Where(c => IsImageFile(c)).ToArray();
+                    if (imageFiles.Length == 0)
+                    {
+                        // 更新 UI（使用 Invoke）
+                        Invoke(new Action(() =>
+                        {
+                            txtResult.AppendText("文件夹中未发现有效图片文件;\r\n");
+                        }));
+                    }
+
+                    Bitmap[] bitmaps = new Bitmap[imageFiles.Length];
+                    for (int i = 0; i < imageFiles.Length; i++)
+                    {
+                        bitmaps[i] = new Bitmap(imageFiles[i]);
+                    }
+
+                    Bitmap output = new Bitmap(bitmaps[0].Width, bitmaps.Select(temp => temp.Height).Sum());
+                    int yy = 0;
+
+                    for (int i = 0; i < bitmaps.Length; i++)
+                    {
+                        for (int y = 0; y < bitmaps[i].Height; y++)
+                        {
+                            for (int x = 0; x < bitmaps[i].Width; x++)
+                            {
+                                var c = bitmaps[i].GetPixel(x, y);
+                                output.SetPixel(x, yy, c);
+                            }
+                            yy++;
+                        }
+                        // 更新 UI（使用 Invoke）
+                        Invoke(new Action(() =>
+                        {
+                            txtResult.AppendText("正在处理第" + (i + 1) + "张图片;\r\n");
+                        }));
+                    }
+
+                    string path = Path.Combine(folder, "合并后图片.jpg");
+                    output.Save(path, ImageFormat.Jpeg);
+
+                    // 释放资源
+                    foreach (var bmp in bitmaps)
+                    {
+                        bmp.Dispose();
+                    }
+                    output.Dispose();
+
+                    // 更新 UI（使用 Invoke）
+                    Invoke(new Action(() =>
+                    {
+                        txtResult.AppendText("处理完成；保存路径：" + path + "；\r\n");
+                    }));
+                });
+            }
+            catch (Exception ex)
+            {
+                txtResult.AppendText("处理时，发生错误，" + ex.Message + ";\r\n");
+            }
+        }
+        bool IsImageFile(string filePath)
+        {
+            // 常见图片扩展名
+            string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp" };
+            string fileExtension = Path.GetExtension(filePath)?.ToLower();
+
+            return imageExtensions.Contains(fileExtension);
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
